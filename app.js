6d3d5364b6079b51a7f3c6f0c0b1dbebb67695d4
Sung Won Cho <mikeswcho@gmail.com>
2015-09-13 10:58:56 +1000

    Add prototype for badge

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var badge = require('./routes/badge');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/b', badge);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
